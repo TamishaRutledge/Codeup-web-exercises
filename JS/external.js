b5cc06c5d8603cbdc91d2e67f0a82ff5f65e9d70
tamisharutledge <tamisha.rutledge@gmail.com>
2018-08-08 17:39:04 -0500

    JavaScript exercises and updates to CSS exercises.

---
+++ JS/external.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// console.log("Hello from external JavaScript.");
+
+// alert("Welcome to my Website!");
+//
+// var userInput = prompt("What is your favorite color?");
+// alert("That's great! My favorite color is " + userInput + " as well.");
+
+
+
+// alert("Trying to calculate the total cost of these movies?");
+// var movie1 = prompt("what was the first movie you picked?");
+// var movie1cost = prompt("And how many days did you have it?");
+// var movie2 = prompt("what was the next movie you picked?");
+// var movie2cost = prompt("And how many days did you have it?");
+// var movie3 = prompt("what was the final movie you picked?");
+// var movie3cost = prompt("And how many days did you have it?");
+// var price = prompt("How much is the price per day?");
+// alert("The total cost of family movie night will be: $" + ((movie1cost * price) + (movie2cost * price) + (movie3cost * price)));
+
+
+// alert("Calculate your pay!");
+// var Google = prompt("How many hours at Google?");
+// var Amazon = prompt("How many hours at Amazon?");
+// var Facebook = prompt("How many hours at Facebook?");
+// alert("Your paycheck for the week: $" + ((Google * 400) + (Amazon * 380) + (Facebook * 350)) + ".00");
+
+
+// var enrollment = ("Check enrollment eligibility.");
+// alert(enrollment);
+// var size =  confirm("Are there available seats in the class?");
+// var conflict = confirm("Will this class fit in your current schedule?");
+// var decider = (size && conflict);
+// alert("It looks the availabilty is: " +  decider);
+
+
+// var offer = ("Do you qualify for a discount?");
+// alert(offer);
+// var items = confirm("Did you buy more than 2 products?");
+// var time = confirm("Is the offer still valid?");
+// var premium = confirm("Are you a Premium member?");
+// var prime = (time && premium);
+// var reg = (items && time);
+// alert("The validity of your coupon is: " + (prime || reg));
---
+++ JS/js_intro.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var age = 32;
+
+
+var message = "Happy Birthday! You are "
+message += ++age;
+message += " years old.";
+console.log(message);
