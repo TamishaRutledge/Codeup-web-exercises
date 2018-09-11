27532a0ffefd7030fe373448f0ca9b324d603735
tamisharutledge <tamisha.rutledge@gmail.com>
2018-09-11 09:09:02 -0500

    Promises exercise.

--- JS/promises.js
+++ JS/promises.js
@@ -1,13 +1,13 @@
 "use strict";
 
-function wait() {
-    return new Promise((resolve, reject) => {
-
-    }
+function wait(n) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, n);
+    });
 }
-
-
-
 
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
