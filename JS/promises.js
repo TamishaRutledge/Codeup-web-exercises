5f9d1a258343553b429a793328fb62468e0684da
tamisharutledge <tamisha.rutledge@gmail.com>
2018-09-11 10:59:05 -0500

    Promises Exercises.

--- JS/promises.js
+++ JS/promises.js
@@ -1,13 +1,29 @@
 "use strict";
 
-function wait(n) {
+function wait(milliSeconds) {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve();
-        }, n);
+        }, milliSeconds);
     });
 }
 
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
+function userCommits(username = 'TamishaRutledge') {
+    return fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${gitToken}`}})
+        .then(response => response.json())
+        .then(data => {
+            for(event of data){
+                if (event.type === "PushEvent") {
+                    let l = event.payload.commits.length;
+                    return event.payload.commits[l -1].url;
+                }
+            }
+        })
+        .then(url => {
+            return fetch(url, {headers: {'Authoriation': `token ${gitToken}`}});
+        })
+}
+ console.log(userCommits());
